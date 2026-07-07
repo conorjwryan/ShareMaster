c80983cc21d1ae11bdef029c0664ec0871cd38fe
Conor Ryan <conor@cjri.uk>
2026-07-07 10:29:40 +0700

    Update sizing.

--- ShareMaster/ShareMasterApp.swift
+++ ShareMaster/ShareMasterApp.swift
@@ -198,7 +198,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let icon = NSImage(named: style.assetName)
             ?? NSImage(systemSymbolName: "paperplane", accessibilityDescription: "ShareMaster")
         icon?.isTemplate = true
-        icon?.size = NSSize(width: 17, height: 17)
+        icon?.size = NSSize(width: 18, height: 18)
         button.image = icon
     }
 
